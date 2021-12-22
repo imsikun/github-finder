562ac5145df34ee9df998e3e31c9ae0477717780
imsikun <imsikun16@gmail.com>
2021-12-22 15:19:18 +0530

    complete project

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,48 @@
+import { Link } from 'react-router-dom';
+
 function About() {
-  return <div>about</div>;
+  return (
+    <div className='container mx-auto'>
+      <div className='about_body'>
+        <div className='mb-8'>
+          <h1 className='mb-4 text-4xl capitalize'>what is this proect?</h1>
+          <p className='capitalize subpixel-antialiased'>
+            this is simply a github finder for the student or in genral for the
+            people.
+          </p>
+        </div>
+        <div className='y_this mb-8'>
+          <h1 className='mb-4 text-4xl capitalize'>y this project made?</h1>
+          <p className='capitalize'>
+            i came across the problem to find the user or the project directly
+            from the github. unless and until the user have a github account
+            they can't able to reach out their destination.
+          </p>
+        </div>
+        <div className='w_benefit mb-8'>
+          <h1 className='mb-4 text-4xl capitalize'>
+            who are going to be benifited?
+          </h1>
+          <p className='capitalize'>
+            lot of people can get the benefit from this including the students
+            who just started and getting to know about the project. a recrutier
+            who wants to have a look at the persons github and unless and until
+            the person doesn't have a github account they couldn't able to see
+            the result.
+          </p>
+        </div>
+        <div className='w_r_u mb-8'>
+          <h1 className='mb-4 text-4xl capitalize'>who made this website?</h1>
+          <p className='capitalize'>
+            to get to more about the person please{' '}
+            <Link to='/author' className='link'>
+              Click Here
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/Pages/Author.jsx
+++ src/Pages/Author.jsx
@@ -1,5 +1,104 @@
+import { ImGithub, ImInstagram, ImLinkedin, ImTwitter } from 'react-icons/im';
+import '../index.css';
 function Author() {
-  return <div>Author</div>;
+  return (
+    <div className='container mx-auto'>
+      <div className='author'>
+        <div className='author_body text-center'>
+          <div className='intro text-2xl'>
+            <h2 className='mb-4'>Hi 👋</h2>
+            <p>
+              This is {''}
+              <a
+                href='https://subhakant.netlify.com'
+                rel='noreferrer'
+                target='_blank'
+                className='link'
+              >
+                Subhakant
+              </a>
+              . I am a Frontend Developer from India.
+            </p>
+          </div>
+          <div className='social flex flex-row justify-center m-4'>
+            <a
+              href='https://linkedin.com/in/subh-mishra'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <ImLinkedin />
+            </a>
+            <a
+              href='https://github.com/imsikun'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <ImGithub />
+            </a>
+            <a
+              href='https://instagram.com/imsikun'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <ImInstagram />
+            </a>
+            <a
+              href='https://twitter.com/imsikun1'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <ImTwitter />
+            </a>
+          </div>
+          <div className='contact_form'>
+            <form
+              id='fs-frm'
+              name='simple-contact-form'
+              accept-charset='utf-8'
+              action='https://formspree.io/f/xqkwzgkj'
+              method='post'
+            >
+              <fieldset id='fs-frm-inputs'>
+                <label for='full-name'>Full Name</label>
+                <input
+                  type='text'
+                  name='name'
+                  id='full-name'
+                  placeholder='First and Last'
+                  required=''
+                />
+                <label for='email-address'>Email Address</label>
+                <input
+                  type='email'
+                  name='_replyto'
+                  id='email-address'
+                  placeholder='email@domain.tld'
+                  required=''
+                />
+                <label for='message'>Message</label>
+                <textarea
+                  rows='3'
+                  name='message'
+                  id='message'
+                  placeholder='Enter your message'
+                  required=''
+                ></textarea>
+                <input
+                  type='hidden'
+                  name='_subject'
+                  id='email-subject'
+                  value='Contact Form Submission'
+                />
+              </fieldset>
+              <div className='text-center'>
+                <input type='submit' value='Submit' />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Author;
